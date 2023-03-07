96aca048a2a81fc77ab769ff4cf8e8598021c69e
cjaradhye <aradhye.swarup@gmail.com>
2023-03-07 21:33:09 +0530

    Completed Website

---
+++ app.js
@@ -0,0 +1,28 @@
+$(".slide1").off().on("click",function(){
+    $(".workshop-globe").attr("src","images/mainglobe-1.png");
+    $(".workshop-smallglobe").attr("src","images/smallglobe-1.png");
+    $(".workshop-smallglobe").css({"left":"1077px","top":"2599px"});
+    $(".workshop-ellipse").css({"width": "192.6px", "height": "563.56px", "left": "302.09px", "top": "1954px"});
+})
+
+$(".slide2").off().on("click",function(){
+    $(".workshop-globe").attr("src","images/mainglobe-2.png");
+    $(".workshop-smallglobe").attr("src","images/smallglobe-2.png");
+    $(".workshop-smallglobe").css({"left":"1208px","top":"2577px"});
+    $(".workshop-ellipse").css({"width": "192.6px", "height": "563.56px", "left": "302.09px", "top": "1954px"});
+
+})
+
+$(".slide3").off().on("click",function(){
+    $(".workshop-globe").attr("src","images/mainglobe-3.png");
+    $(".workshop-smallglobe").attr("src","images/smallglobe-3.png");
+    $(".workshop-smallglobe").css({"left":"1255px","top":"2708px"});
+    $(".workshop-ellipse").css({"width": "181.6px", "height": "449px","left": "208.09px","top": "2018px"});
+})
+
+$(".slide4").off().on("click",function(){
+    $(".workshop-globe").attr("src","images/mainglobe-4.png");
+    $(".workshop-smallglobe").attr("src","images/smallglobe-4.png");
+    $(".workshop-smallglobe").css({"left":"1152px","top":"2784px"});
+    $(".workshop-ellipse").css({"width": "146.6px", "height": "362px","left": "171.09px","top": "2059px"});
+})
